354159d37ae94c1fe4cb5e0489d556b2be6163fd
rajatgupta19999 <rajatgupta19999@gmail.com>
2023-05-31 15:33:11 +0100

    deployed version

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
-# syntax=docker/dockerfile:1
-
 FROM python:3.11-bullseye
-
 WORKDIR /work
 
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+RUN pip3 --timeout=100 --no-cache-dir install 'transformers[torch]'
+RUN pip3 --no-cache-dir install fastapi
+RUN pip3 --no-cache-dir install uvicorn
+
+WORKDIR /work
 
 COPY ./p2_infer.py ./p2_infer.py
 COPY ./P2.0-model ./P2.0-model
 
 
 CMD ["uvicorn", "p2_infer:app" , "--host", "0.0.0.0", "--port", "80"]]
-
-
--- p2_infer.py
+++ p2_infer.py
@@ -1,9 +1,11 @@
 ## Inferencing ...
+# !pip install torch torchvision torchaudio transformers fastapi uvicorn
 
 import os
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+#import nest_asyncio
 
 app = FastAPI()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -16,7 +18,7 @@ class Input_Text(BaseModel):
     _id: str
     text: str
 
-device = torch.device("mps")
+device = torch.device("cpu")
 
 tokenizer = AutoTokenizer.from_pretrained("./P2.0-model/tokenizer")
 model = AutoModelForCausalLM.from_pretrained("./P2.0-model/")
@@ -40,3 +42,20 @@ async def create_item(text_input:Input_Text):
 # Run this file on command prompt with "uvicorn p2_infer:app --reload" command. Here p2_infer is the file name and app is the object we created in this file
 
 # Go to http://127.0.0.1:8000/docs to access a swagger like UI to access the endpoimts
+# docker build --platform=linux/amd64 -t gcr.io/terraform-gke-openvino/text-gen-amd:base . 
+#docker build -t gcr.io/terraform-gke-openvino/text-gen:base .     --> gcr.io/<project ID>/<name> Specific to how GKE wants the image name 
+#docker run -it -p 80:80 gcr.io/terraform-gke-openvino/text-gen-amd:base
+#docker push gcr.io/terraform-gke-openvino/text-gen-amd:base
+
+#gcloud container clusters get-credentials text-gen-cluster \
+ #   --region us-central1
+
+"""
+kubectl apply -f build.yaml
+ kubectl logs deployment/<name-of-deployment> # logs of deployment
+kubectl logs -f deployment/yolo
+kubectl logs -f deployment/<name-of-deployment> # follow logs
+kubectl expose deployment yolo --type LoadBalancer --port 80 --target-port 80
+ """
+
+#curl -v http://localhost:50/items/1
